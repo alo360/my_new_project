f7e4771dd32bd9199f1e80db14e8e26a7a449cd0
Chilton <nguyenthienchi110@gmail.com>
2021-05-12 20:03:28 +0700

    add more for app.py

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
 print('Hello world')
+# git init
+# git status
+# git add .
+# git commit -m "xxx"
+# git log
+# git branch -a
+# git branch my_new_branch
+# git checkout my_new_branch
+# git merge other_branch
